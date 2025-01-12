d621b339d010eea700ee55d80dd4b48732534cc9
sagar610 <sagargondaliya50@gmail.com>
2025-01-12 23:46:12 +0000

    Auto commit: 12/01/2025 23:46:11.60

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Leaderboard = ({ leaderboard = [] }) => {
   return (
     <div className="leaderboard w-full h-full bg-gray-800 p-4 rounded-lg shadow-lg text-white overflow-y-scroll">
-      <h2 className="text-xl font-semibold text-center mb-4">Leaderboard</h2>
+      <h2 className="text-xl font-semibold text-center text-black mb-5 rounded-lg bg-yellow-300">Leaderboard</h2>
       {leaderboard.length === 0 ? (
         <p className="text-center text-lg">No scores yet.</p>
       ) : (
